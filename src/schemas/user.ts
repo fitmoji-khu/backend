3ff24b9d585a8217d51bd0819bdbc9af881c98ca
seoeunbi25 <seoeunbi25@naver.com>
2025-08-17 03:16:29 +0900

    refactor: Rename variables in user and userInfo schemas

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,11 +1,10 @@
 import S, { type JSONSchema } from 'fluent-json-schema';
 import commonSchema from "./common";
-import userSchema from './user';
 import { User } from '../lib/type';
 
 export default {
     id: commonSchema.id,
-    email: userSchema.email,
+    email: commonSchema.email,
     password: S.string(),
     name: S.string(),
     image: S.string()
--- src/schemas/userInfo.ts
+++ src/schemas/userInfo.ts
@@ -1,11 +1,11 @@
 import S, { type JSONSchema } from "fluent-json-schema";
 import commonSchema from "./common";
-import userSchema from './user';
+import userSchema from './user'
 import { UserInfo } from '../lib/type';
 
 export default {
     id: commonSchema.id,
-    userId: userSchema.id,
+    user_id: userSchema.id,
     personal_color: S.string(),
     style: S.string(),
     height: S.integer(),
