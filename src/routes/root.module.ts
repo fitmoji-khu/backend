b858a707680ae858dd05c5ec85cf621953b2c7b1
seoeunbi25 <seoeunbi25@naver.com>
2025-08-27 22:21:38 +0900

    feat: Add medias module to root module

--- src/routes/root.module.ts
+++ src/routes/root.module.ts
@@ -1,7 +1,9 @@
 import Module from '../lib/module';
 import usersModule from './users/users.module';
 import authModule from './auth/auth.module';
+import mediasModule from './medias/medias.module';
 
 export default new Module('')      
     .addModule(usersModule)
     .addModule(authModule)
+    .addModule(mediasModule)
