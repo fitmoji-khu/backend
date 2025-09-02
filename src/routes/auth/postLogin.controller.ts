aa6b135d17097fd6a75dde4894ad8f635ce2eaf4
seoeunbi25 <seoeunbi25@naver.com>
2025-09-03 01:17:20 +0900

    refactor: Improve code structure of the auth module

--- src/routes/auth/postLogin.controller.ts
+++ src/routes/auth/postLogin.controller.ts
@@ -8,38 +8,42 @@ import { BadRequest } from '../../lib/httpError';
 export default async function (request: FastifyRequest<{
     Body: Pick<User, 'email' | 'password'>;
 }>, reply: FastifyReply): Promise<void> {
-    await prisma.$transaction(async (transaction): Promise<void> => {
-        const user = await transaction.user.findFirst({
-            where: {
-                email: request['body']['email'],
-                deleted_at: null
-            },
-            select: {
-                id: true,
-                password: true
+    try {
+        const token = await prisma.$transaction(async (transaction): Promise<{ accessToken: string, refreshToken: string }> => {
+            const user = await transaction.user.findFirst({
+                where: {
+                    email: request['body']['email'],
+                    deleted_at: null
+                },
+                select: {
+                    id: true,
+                    password: true
+                }
+            });
+
+            if (user === null) {
+                throw new BadRequest('Body["email"] and Body["password"] must be valid');
             }
+
+            const password = await verifyPassword(request['body']['password'], user['password']);
+            if (password !== true) {
+                throw new BadRequest('Body["password"] must be valid');
+            }
+            
+            const [accessToken, refreshToken] = await Promise.all([
+                signAccessToken(user['id'], '15m'),
+                signRefreshToken(user['id'], '7d')
+            ]);
+            return { accessToken, refreshToken } 
         });
-
-        if (user === null) {
-            throw new BadRequest('Body["email"] and Body["password"] must be valid');
-        }
-
-        const password = await verifyPassword(request['body']['password'], user['password']);
-        if (password !== true) {
-            throw new BadRequest('Body["password"] must be valid');
-        }
-        
-        const [accessToken, refreshToken] = await Promise.all([
-            signAccessToken(user['id'], '15m'),
-            signRefreshToken(user['id'], '7d')
-        ]);
-
         reply.status(201)
             .send({
                 token: {
-                    access: accessToken,
-                    refresh: refreshToken
+                    access: token['accessToken'],
+                    refresh: token['refreshToken']
                 }
             });
-    });
+    } catch(err) {
+        throw err;
+    }
 }
--- src/routes/auth/postToken.controller.ts
+++ src/routes/auth/postToken.controller.ts
@@ -8,31 +8,36 @@ export default async function (request: FastifyRequest<{
         refreshToken: string;
     };
 }>, reply: FastifyReply): Promise<void> {
-    await prisma.$transaction(async (transaction): Promise<void> => {
-        const user = await transaction.user.findFirst({
-            where: {
-                id: request['userId'],
-                deleted_at: null
+    try {
+        const token = await prisma.$transaction(async (transaction): Promise<{ accessToken: string, refreshToken: string }> => {
+            const user = await transaction.user.findFirst({
+                where: {
+                    id: request['userId'],
+                    deleted_at: null
+                }
+            })
+            if (user === null) {
+                throw new BadRequest('Request["userId"] must be valid');
             }
-        })
-        if (user === null) {
-            throw new BadRequest('Request["userId"] must be valid');
-        }
-        
-        const payload = await verifyRefreshToken(request['body']['refreshToken']);
-        const userId = Number.parseInt(payload['sub']);
+            
+            const payload = await verifyRefreshToken(request['body']['refreshToken']);
+            const userId = Number.parseInt(payload['sub']);
 
-        const [accessToken, refreshToken] = await Promise.all([
-            signAccessToken(userId, '15m'),
-            signRefreshToken(userId, '7d')
-        ]);
+            const [accessToken, refreshToken] = await Promise.all([
+                signAccessToken(userId, '15m'),
+                signRefreshToken(userId, '7d')
+            ]);
 
+            return { accessToken, refreshToken }
+        });
         reply.status(201)
             .send({
                 token: {
-                    access: accessToken,
-                    refresh: refreshToken
+                    access: token['accessToken'],
+                    refresh: token['refreshToken']
                 }
             });
-    });
+    } catch(err) {
+        throw err;
+    }
 }
